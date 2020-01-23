fa58c15f958b2deec64ffe7106902d179b1ae3f3
Renato Querino <renatoqrn@gmail.com>
2020-01-22 21:30:18 -0800

    ex01 ok

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rquerino <rquerino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 16:44:41 by rquerino          #+#    #+#             */
-/*   Updated: 2020/01/22 17:56:21 by rquerino         ###   ########.fr       */
+/*   Updated: 2020/01/22 21:29:40 by rquerino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ int     randomAlphaNum(void) {
     //num: 48-57
     //upperAlpha: 65-90
     //lowerAlpha: 97-122
-    srand (time(NULL));
     nbr1 = rand() % 3;
     if (nbr1 == 0)
         nbr2 = rand() % 10 + 48;
@@ -35,7 +34,7 @@ int     randomAlphaNum(void) {
         nbr2 = rand() % 25 + 65;
     else
         nbr2 = rand() % 25 + 97;
-    return nbr2;
+	return nbr2;
 }
 
 void	*serialize(void)
@@ -56,12 +55,18 @@ void	*serialize(void)
 	data->s2.assign(s2);
     
     std::cout << "Serializing:" << std::endl;
-    std::cout << "s1: " << data->s1 << std::endl;
-    std::cout << "n:  " << data->n << std::endl;
-    std::cout << "s2: " << data->s2 << std::endl;
+    std::cout << "Random string 1: " << data->s1 << std::endl;
+    std::cout << "Random number:  " << data->n << std::endl;
+    std::cout << "Random string 2: " << data->s2 << std::endl;
 
 	return (reinterpret_cast<void*>(data));
 }
+
+// It is used to convert one pointer of another pointer of any type, no matter either the class
+// is related to each other or not. 
+// It does not check if the pointer type and data pointed by the pointer is same or not.
+// It can typecast any pointer to any other data type and is only used to typecast any pointer to its original type.
+// https://www.geeksforgeeks.org/reinterpret_cast-in-c-type-casting-operators/
 
 Data	*deserialize(void * raw)
 {
@@ -74,90 +79,13 @@ int main()
 	Data *b;
 
 	a = serialize();
+	std::cout << std::endl;
+	std::cout << "Address of random serialized Data: " << a << std::endl;
 	b = deserialize(a);
-
     std::cout << "Deserializing:" << std::endl;
 	std::cout << "s1: " << b->s1 << std::endl;
 	std::cout << "n:  " << b->n << std::endl;
 	std::cout << "s2: " << b->s2 << std::endl;
 
 	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-void 	*serialize(void)
-{
-	int 	i = -1;
-	char	*data = new char[20];
-
-	char	output[] =
-	{
-		"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	};
-	
-	std::cout << "Serialized of Concatenated Arrays/Int Being Made: " << std::endl;
-	while (++i < 8)
-		data[i] = output[rand() % 62];
-	i = 7;
-	while (++i < 12)
-		data[i] = rand() % 256 - 128;
-	i = 11;
-	while (++i < 20)
-		data[i] = output[rand() % 62];
-	data[i] = '\0';
-	i = -1;
-	std::cout << "Random Array 1: ";
-	while (++i < 8)
-		std::cout << data[i];
-	std::cout << std::endl << "Random Integer: " << static_cast<int>(data[8]);
-	i = 11;
-	std::cout << std::endl << "Random Array 2: ";
-	while (++i < 20)
-		std::cout << data[i];
-	std::cout << std::endl;
-	return (reinterpret_cast<void*>(data));
-}
-
-Data 	*deserialize(void	*raw)
-{
-	Data 		*data = new Data;
-
-	data->n = 0;
-	char		*input = reinterpret_cast<char*>(raw);
-	int i = -1;
-
-	while (++i < 8)
-		data->s1 = data->s1 + input[i];
-	data->n = input[8];
-	i = 11;
-	while (++i < 20)
-		data->s2 = data->s2 + input[i];
-	return (data);
-}
-
-int	main()
-{
-	void	*serial;
-	Data	*deserial;
-
-	srand(time(0));
-	serial = serialize();
-	std::cout << "Address of the random serialized data" << std::endl << serial << std::endl << std::endl;
-	deserial = deserialize(serial);
-	std::cout << "Results of deserializing the serialized data using the address" << std::endl <<
-		"Random Array 1: " << deserial->s1 << std::endl << 
-		"Random Integer: " << deserial->n << std::endl << 
-		"Random Array 2: " << deserial->s2 << std::endl;
-
-	delete deserial;
-
-	return (0);
 }
